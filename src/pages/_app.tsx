59a146c8f98a9b0ddf89e8a5ad50e34d2aa04c7b
mxkmln <1993mr237@gmail.com>
2024-10-13 12:53:12 +0530

    v10

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/manifest.json" />
         <meta name="theme-color" content="#ffffff" />
-        <title>Your App Name</title>
+        <title>Muskan</title>
       </Head>
       <Component {...pageProps} />
     </>
