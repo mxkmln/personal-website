a558be8046bae8069795344576fa55e6596732dd
mxkmln <1993mr237@gmail.com>
2025-04-04 18:58:21 +0530

    minor homepage changes

--- src/components/Exploration.tsx
+++ src/components/Exploration.tsx
@@ -1,11 +1,11 @@
 export default function Exploration() {
-  const interests = ['AI', 'B2B SaaS', 'Bharat', 'Automation', 'Retail']
+  const interests = ['AI', 'Logistics', 'Bharat', 'Automation', 'Retail']
 
   return (
     <section className="mb-12">
       <h2 className="text-2xl mb-4 font-bold">Explorations</h2>
       <p className="mb-4 text-sm md:text-base">
-        Looking to build a company at the crossover of tech and ops. Particularly interested in India-focused businesses and how best to leverage tech for that. Love to discuss ideas around offline-first businesses and B2B SaaS focusing on AI-led automation. Also studying scientific philosophy on the side.
+        Looking to build a company at the crossover of tech and ops. Particularly interested in India-focused businesses and how best to leverage tech for that. Love to discuss ideas around offline-first businesses leveraging tech. Also studying scientific philosophy on the side.
       </p>
       <div className="flex flex-wrap gap-2">
         {interests.map((interest, index) => (
