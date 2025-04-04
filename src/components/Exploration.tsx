1f74fd2ef49ed95ed8fd6881eb453eca499862ec
mxkmln <1993mr237@gmail.com>
2025-04-05 00:41:58 +0530

    boo

--- src/components/Exploration.tsx
+++ src/components/Exploration.tsx
@@ -5,7 +5,7 @@ export default function Exploration() {
     <section className="mb-12">
       <h2 className="text-2xl mb-4 font-bold">Explorations</h2>
       <p className="mb-4 text-sm md:text-base">
-        Looking to build a company at the crossover of tech and ops. Particularly interested in India-focused businesses and how best to leverage tech for that. Love to discuss ideas around offline-first businesses. Also like to read psychology and scientific philosophy on the side.
+        Looking to build a company at a heavy crossover of tech and ops. Particularly interested in India-focused businesses and how best to leverage tech for that. No 10 minute anything, though I am an avid user. Love to discuss ideas around offline-first businesses. Also like to read psychology and scientific philosophy on the side.
       </p>
       <div className="flex flex-wrap gap-2">
         {interests.map((interest, index) => (
